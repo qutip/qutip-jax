200743894fa31a1fa02c8b585f8d6dd4b51ef900
quantshah <shahnawaz.ahmed95@gmail.com>
2022-09-30 14:34:05 +0200

    conversion to jax type from Qobj

--- src/qutip_jax/__init__.py
+++ src/qutip_jax/__init__.py
@@ -1,23 +1,17 @@
 import qutip
 from qutip_jax.jaxarray import JaxArray
 
-from .convert import jax_array_from_dense, jax_array_to_dense, is_jax_array
+from .convert import jax_from_dense, dense_from_jax
 from .version import version as __version__
 
 
 # Register the data layer for JAX
 qutip.data.to.add_conversions(
     [
-        (JaxArray, qutip.data.Dense, jax_array_from_dense),
-        (qutip.data.Dense, JaxArray, jax_array_to_dense),
+        (JaxArray, qutip.data.Dense, jax_from_dense),
+        (qutip.data.Dense, JaxArray, dense_from_jax)
     ]
 )
 
 # User friendly name for conversion with `to` or Qobj creation functions:
-qutip.data.to.register_aliases(["JaxArray"], JaxArray)
-
-qutip.data.create.add_creators(
-    [
-        (is_jax_array, JaxArray, 85),
-    ]
-)
+qutip.data.to.register_aliases(["jax"], JaxArray)
--- src/qutip_jax/convert.py
+++ src/qutip_jax/convert.py
@@ -5,11 +5,10 @@ import jax.numpy as jnp
 import numpy as np
 
 # Conversion function
-def jax_array_from_dense(dense):
-    return JaxArray(dense.data.to_array())
+def jax_from_dense(dense):
+    return qutip.data.Dense(dense.to_array(), copy=False)
 
-
-def jax_array_to_dense(jax_array):
+def dense_from_jax(jax_array):
     return qutip.data.Dense(np.array(jax_array), copy=False)
 
 
