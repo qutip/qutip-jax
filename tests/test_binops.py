c11d8fc25d20ee41468425382a7dddd5786c44fc
quantshah <shahnawaz.ahmed95@gmail.com>
2022-10-07 11:56:04 +0200

    removed JIT tests

--- tests/test_binops.py
+++ tests/test_binops.py
@@ -19,27 +19,9 @@ class TestAdd(testing.TestAdd):
             qutip_jax.add_jaxarray,
             qutip_jax.JaxArray,
             qutip_jax.JaxArray,
-            qutip_jax.JaxArray
+            qutip_jax.JaxArray,
         )
     ]
-
-    def test_jit_from_jxa(self):
-        """Test JIT of add using the _jxa array"""
-
-        @jax.jit
-        def func():
-            return sigmax().to("jax").data._jxa + sigmay().to("jax").data._jxa
-
-        assert isinstance(func(), jax.interpreters.xla.DeviceArray)
-
-    def test_jit_from_qobj(self):
-        """Test JIT of add directly using Qobj"""
-
-        @jax.jit
-        def func():
-            return sigmax() + sigmay()
-
-        assert isinstance(func(), jax.interpreters.xla.DeviceArray)
 
 
 class TestSub(testing.TestSub):
@@ -48,18 +30,16 @@ class TestSub(testing.TestSub):
             qutip_jax.sub_jaxarray,
             qutip_jax.JaxArray,
             qutip_jax.JaxArray,
-            qutip_jax.JaxArray
+            qutip_jax.JaxArray,
         )
     ]
 
+
 class TestMul(testing.TestMul):
     specialisations = [
-        pytest.param(
-            qutip_jax.mul_jaxarray,
-            qutip_jax.JaxArray,
-            qutip_jax.JaxArray
-        )
+        pytest.param(qutip_jax.mul_jaxarray, qutip_jax.JaxArray, qutip_jax.JaxArray)
     ]
+
 
 class TestMatmul(testing.TestMatmul):
     specialisations = [
@@ -67,9 +47,10 @@ class TestMatmul(testing.TestMatmul):
             qutip_jax.matmul_jaxarray,
             qutip_jax.JaxArray,
             qutip_jax.JaxArray,
-            qutip_jax.JaxArray
+            qutip_jax.JaxArray,
         )
     ]
+
 
 class TestMultiply(testing.TestMultiply):
     specialisations = [
@@ -77,9 +58,10 @@ class TestMultiply(testing.TestMultiply):
             qutip_jax.multiply_jaxarray,
             qutip_jax.JaxArray,
             qutip_jax.JaxArray,
-            qutip_jax.JaxArray
+            qutip_jax.JaxArray,
         )
     ]
+
 
 class TestKron(testing.TestKron):
     specialisations = [
@@ -87,15 +69,12 @@ class TestKron(testing.TestKron):
             qutip_jax.kron_jaxarray,
             qutip_jax.JaxArray,
             qutip_jax.JaxArray,
-            qutip_jax.JaxArray
+            qutip_jax.JaxArray,
         )
     ]
 
+
 class TestPow(testing.TestPow):
     specialisations = [
-        pytest.param(
-            qutip_jax.pow_jaxarray,
-            qutip_jax.JaxArray,
-            qutip_jax.JaxArray
-        )
+        pytest.param(qutip_jax.pow_jaxarray, qutip_jax.JaxArray, qutip_jax.JaxArray)
     ]
