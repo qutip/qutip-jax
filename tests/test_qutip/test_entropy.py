dcda470789f25e2150aaab1032da6aafdaecaf9d
Rochisha Agarwal <rochisha.agarwal2302@gmail.com>
2024-08-08 13:26:22 +0530

    made required changes to test_entropy

--- tests/test_qutip/test_entropy.py
+++ tests/test_qutip/test_entropy.py
@@ -13,35 +13,28 @@ with qutip.CoreOptions(default_dtype="jax"):
     basis_0 = qutip.basis(2, 0)
     basis_1 = qutip.basis(2, 1)
     bell_state = (qutip.tensor(basis_0, basis_1) + qutip.tensor(basis_1, basis_0)).unit()
-    density_matrix = bell_state * bell_state.dag()
+    bell_dm = bell_state * bell_state.dag()
     dm = qutip.rand_dm([5, 5], distribution="pure")
 
-with qutip.CoreOptions(default_dtype="jax"):
-    X = qutip.sigmax()
-    I = qutip.qeye(2)
-    CNOT = qutip.tensor(qutip.basis(2, 0) * qutip.basis(2, 0).dag(), I) + qutip.tensor(qutip.basis(2, 1) * qutip.basis(2, 1).dag(), X)
-
 @pytest.mark.parametrize("func, name, args", [
-    (entropy_vn, "entropy_vn", (density_matrix,)),
-    (entropy_linear, "entropy_linear", (density_matrix,)),
-    (concurrence, "concurrence", (density_matrix,)),
-    (participation_ratio, "participation_ratio", (density_matrix,))
+    (entropy_vn, "entropy_vn", (bell_dm,)),
+    (entropy_linear, "entropy_linear", (bell_dm,)),
+    (concurrence, "concurrence", (bell_dm,)),
+    (participation_ratio, "participation_ratio", (bell_dm,))
 ])
 
 def test_jit(func, name, args):
     func_jit = jit(func)
     result = func(*args)
     result_jit = func_jit(*args)
-    print(f"{name} (original):", result)
-    print(f"{name} (JIT):", result_jit)
     assert jnp.abs(result - result_jit) < tol
 
 @pytest.mark.parametrize("func, name, args", [
-    (entropy_vn, "entropy_vn", (density_matrix,)),
-    (entropy_linear, "entropy_linear", (density_matrix,)),
+    (entropy_vn, "entropy_vn", (bell_dm,)),
+    (entropy_linear, "entropy_linear", (bell_dm,)),
     (entropy_mutual, "entropy_mutual", (dm, [0], [1])),  
-    (concurrence, "concurrence", (density_matrix,)),
-    (entropy_conditional, "entropy_conditional", (density_matrix, 0)),
+    (concurrence, "concurrence", (bell_dm,)),
+    (entropy_conditional, "entropy_conditional", (bell_dm, 0)),
     #(entangling_power, "entangling_power", (CNOT)),
 ])
 def test_grad(func, name, args):
