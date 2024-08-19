b8117b4870ef569df7310c0d77f67615d1c52a71
Rochisha Agarwal <rochisha.agarwal2302@gmail.com>
2024-08-20 00:30:33 +0530

    update settings on change of backend

--- src/qutip_jax/settings.py
+++ src/qutip_jax/settings.py
@@ -1,7 +1,22 @@
 import jax.numpy as jnp
+import numpy as np
 from qutip import settings
+from qutip import SESolver, MCSolver, MESolver
 
 __all__ = ["use_jax_backend"]
 
 def use_jax_backend():
     settings.core['numpy_backend'] = jnp
+    settings.core['default_dtype'] = 'jax'
+    SESolver.solver_options['method'] = 'diffrax'
+    MESolver.solver_options['method'] = 'diffrax'
+    MCSolver.solver_options['method'] = 'diffrax'
+
+
+def use_numpy_backend():
+    settings.core['numpy_backend'] = np
+    settings.core['default_dtype'] = None
+    SESolver.solver_options['method'] = 'adams'
+    MESolver.solver_options['method'] = 'adams'
+    MCSolver.solver_options['method'] = 'adams'
+
